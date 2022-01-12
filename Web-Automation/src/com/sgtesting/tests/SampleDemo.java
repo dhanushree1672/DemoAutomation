6171caf1994ba53f046e1a4985f5318a709e0fbd
Dhanushree <dhanushree1672@gmail.com>
2022-01-12 10:49:44 -0800

    Even Numbers

---
+++ Web-Automation/src/com/sgtesting/tests/SampleDemo.java
@@ -0,0 +1,16 @@
+package com.sgtesting.tests;
+
+public class SampleDemo {
+
+	public static void main(String[] args) {
+		for(int i=20;i<=40;i++)
+		{
+			if(i%2==0)
+			{
+				System.out.println("The even numbers:"+i);
+			}
+		}
+
+	}
+
+}
