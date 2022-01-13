ae120f143312bc7036cf849efa14ba67323b58c6
Dhanushree <dhanushree1672@gmail.com>
2022-01-12 19:31:39 -0800

    Pattern Design

---
+++ Web-Automation/src/com/sgtesting/SamplePrograms/Pattern.java
@@ -0,0 +1,20 @@
+package com.sgtesting.SamplePrograms;
+
+public class Pattern {
+	public static void main(String[] args) {
+		int i=1;
+		while(i<=6)
+		{
+			int j=1;
+			while(j<=3)
+			{
+				System.out.print(" @ ");
+				j++;
+			}
+
+			System.out.println();
+			i++;
+		}
+	}
+
+}
